b0b7b357fc80c0ab286bc02c4f2c57cf698cbce7
Vitaliy <v@aminev.me>
2013-06-24 16:33:39 +0400

    added test

--- test/test.js
+++ test/test.js
@@ -4,4 +4,8 @@ describe("test 2 + 1", function() {
 	it("should 2 + 1 is 3", function () {
 		assert.equal(2 + 1, 3);
 	});
+
+	it("should 2 * 3 is 6", function () {
+		assert.equal(2 * 3, 6);
+	});
 });
