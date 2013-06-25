e56e2357071e75da233abee4c23bcc9f0e9ac07e
avvs <v@aminev.me>
2013-06-25 15:55:50 +0400

    wrong test

--- test/test.js
+++ test/test.js
@@ -8,4 +8,8 @@ describe("test 2 + 1", function() {
 	it("should 2 * 3 is 6", function () {
 		assert.equal(2 * 3, 6);
 	});
+
+	it("should 3 * 5 is 15", function () {
+		assert.equal(3 * 5, 15);
+	});
 });
